ad95b8dfe34437328000bec3fa69393dca7211dd
Papin Nikolai <nikolai@weirdcat.ru>
2024-05-15 12:10:29 +0300

    Initialized configurationservice

---
+++ ConfigurationService/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.Run();
