dba8e78bb9e25c9e67ac43fb744cd742a0c12229
Papin Nikolai <nikolai@weirdcat.ru>
2024-05-19 12:31:41 +0300

    Working on nginx api-gateway, mapped all microservices including future ones

--- APIGateway/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddEnvironmentVariables();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-app.Run();
