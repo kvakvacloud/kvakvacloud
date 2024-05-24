b0ec44e978979c443fc2d81e1e45902493c21c97
Papin Nikolai <nikolai@weirdcat.ru>
2024-05-24 16:21:39 +0300

    Added authorization setup

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,14 +1,19 @@
-using System.Reflection;
+using System.Text;
 using AuthService.Database;
+
 using AuthService.Repository;
 using AuthService.Services;
 using AuthService.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Configuration.AddEnvironmentVariables();
 
+// Database
 builder.Services.AddDbContext<ApplicationContext>(x => {
     var Hostname=Environment.GetEnvironmentVariable("DB_HOSTNAME") ?? "authdb";
     var Port=Environment.GetEnvironmentVariable("DB_PORT") ?? "5432";
@@ -18,11 +23,34 @@ builder.Services.AddDbContext<ApplicationContext>(x => {
     x.UseNpgsql($"Server={Hostname}:{Port};Database={Name};Uid={Username};Pwd={Password};");
 });
 
+// Services
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IRegistrationCodeRepository, RegistrationCodeRepository>();
 builder.Services.AddScoped<IJwtUtils, JwtUtils>();
 builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<IMicroserviceAuthService, MicroserviceAuthService>();
+
+// Authorization
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+
+        string issuer = Environment.GetEnvironmentVariable("AUTH_JWT_ISSUER") ?? "authservice";
+        string audience = Environment.GetEnvironmentVariable("AUTH_JWT_AUDIENCE") ?? "authservice";
+        string secret = Environment.GetEnvironmentVariable("AUTH_JWT_SECRET") ?? "TopSecretKeyForTheProtectionOfChocolateCookiesAndOtherSweetThings";
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = issuer,
+            ValidateAudience = true,
+            ValidAudience = audience,
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+            ValidateIssuerSigningKey = true
+        };
+    });
 
 builder.Services.AddControllers();
 
@@ -39,6 +67,32 @@ builder.Services.AddSwaggerGen(c =>
         },
     });
 
+    // Add JWT Bearer authentication
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        BearerFormat = "JWT",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+
+
     var xmlFile = Path.Combine(AppContext.BaseDirectory, "TestAPI.xml");
     if (File.Exists(xmlFile))
     {
@@ -52,15 +106,17 @@ var app = builder.Build();
 app.UseRouting();
 app.MapControllers();
 
-app.UseSwagger(c =>
+if (app.Environment.IsDevelopment())
 {
-    c.RouteTemplate = "/Auth/swagger/{documentName}/swagger.json";
-});
-app.UseSwaggerUI(c =>
-{
-    c.RoutePrefix = "Auth";
-    //c.SwaggerEndpoint("", "Sample API");
-    c.SwaggerEndpoint("/Auth/swagger/v1/swagger.json", "v1");
-});
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "/Auth/swagger/{documentName}/swagger.json";
+    });
+    app.UseSwaggerUI(c =>
+    {
+        c.RoutePrefix = "Auth";
+        c.SwaggerEndpoint("/Auth/swagger/v1/swagger.json", "v1");
+    });
+}
 
 app.Run();
